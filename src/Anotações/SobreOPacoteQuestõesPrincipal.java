b0e330abbc6f3d0f202a73e2379f255b071fb8f7
Gabriel Italo <103376239+Italo-Contato@users.noreply.github.com>
2022-07-05 11:36:18 -0300

    first commit

---
+++ src/Anotações/SobreOPacoteQuestõesPrincipal.java
@@ -0,0 +1,13 @@
+package Anotações;
+
+public @interface SobreOPacoteQuestõesPrincipal {
+	/*Dentro do pacote QuestôesPrincipal estão duas classe importantes
+	 *
+	 *1ºA classe Escolhe questôes é a que vai chamar cada classe de acordo com a demanda
+	 * 
+	 *2ºJá a classe QuestõesPrincipal é nossa main, e vai chamar apenas a classe EscolheQuestões
+	 * com seu método que faz a escolha.
+	 * 
+	 * 
+	 */
+}
